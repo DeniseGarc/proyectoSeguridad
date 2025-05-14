0ba64796c02027aee598c87c3e1f62000038db5b
DeniseGarc <106357472+DeniseGarc@users.noreply.github.com>
2025-05-14 01:00:11 -0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt');
 const sqlite3 = require('sqlite3').verbose();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Conexión a la base de datos
 const db = new sqlite3.Database('./mydatabase.db', (err) => {
